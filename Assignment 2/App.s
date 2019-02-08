da888445bff1fcad349cecb6a26fdb93f5fa4a6f
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-02-08 13:40:12 +0000

    Get Decimal Subroutine
    Implemented the get decimal subroutine. This converts an unsigned hex number to its corresponding decimal representation

--- Assignment 2/App.s
+++ Assignment 2/App.s
@@ -8,33 +8,33 @@
 start
 
 ;Setup pin addresses
-IO1DIR	EQU	0xE0028018
-IO1SET	EQU	0xE0028014
-IO1CLR	EQU	0xE002801C
-
-	ldr	r1,=IO1DIR
-	ldr	r2,=0x000f0000	;select P1.19--P1.16
-	str	r2,[r1]		;make them outputs
-	ldr	r1,=IO1SET
-	str	r2,[r1]		;set them to turn the LEDs off
-	ldr	r2,=IO1CLR
+;IO1DIR	EQU	0xE0028018
+;IO1SET	EQU	0xE0028014
+;IO1CLR	EQU	0xE002801C
+;
+;	ldr	r1,=IO1DIR
+;	ldr	r2,=0x000f0000	;select P1.19--P1.16
+;	str	r2,[r1]		;make them outputs
+;	ldr	r1,=IO1SET
+;	str	r2,[r1]		;set them to turn the LEDs off
+;	ldr	r2,=IO1CLR
 ; r1 points to the SET register
 ; r2 points to the CLEAR register
 
-	ldr	r5,=0x00100000	; end when the mask reaches this value
-wloop	ldr	r3,=0x00010000	; start with P1.16.
-floop	str	r3,[r2]	   	; clear the bit -> turn on the LED
+;	ldr	r5,=0x00100000	; end when the mask reaches this value
+;wloop	ldr	r3,=0x00010000	; start with P1.16.
+;floop	str	r3,[r2]	   	; clear the bit -> turn on the LED
 
 ;delay for about a half second
-	ldr	r4,=4000000
-dloop	subs	r4,r4,#1
-	bne	dloop
+;	ldr	r4,=4000000
+;dloop	subs	r4,r4,#1
+;	bne	dloop
 
-	str	r3,[r1]		;set the bit -> turn off the LED
-	mov	r3,r3,lsl #1	;shift up to next bit. P1.16 -> P1.17 etc.
-	cmp	r3,r5
-	bne	floop
-	b	wloop
+;	str	r3,[r1]		;set the bit -> turn off the LED
+;	mov	r3,r3,lsl #1	;shift up to next bit. P1.16 -> P1.17 etc.
+;	cmp	r3,r5
+;	bne	floop
+;	b	wloop
 	
 	;
 	;
@@ -44,24 +44,58 @@ dloop	subs	r4,r4,#1
 	;
 	;
 	
+	ldr r0, =0x0000101B
+	ldr r1, =ASCIIREPRESENTATION
+	ldr r2, =DIVISORTABLE
+	BL getDecimal
 	
+
 	
 stop	B	stop
 
-	END
+		
 
-;Convert to Ascii Subroutine
-;Then converts this to the ascii code for that number
-;note only need to consider last 4bits 
+;Convert to Decimal Subroutine
+;Converts a Unsigned interger to its decimal digits
+;and saves it in memory
 ;
-;R0 - number being masked
-;R1 - memory address of space for ascii characters
+;R0 - number being converted
+;R1 - memory address of space for decimal digits
 ;R2 - memory address of divisor table
-;R0 - return ascii character
+;
 
-	stmfd SP!, {lr, r3-r8}
+getDecimal
+	stmfd SP!, {lr, r3-r8}				; Store registers to stack
 	
-	ldmfd SP!, {pc, r3-r8}
+	ldr r4, =0 							; Divisor table index = 0;
+for
+	cmp r4, #10							; for(index < divisorTable.length){
+	bge endfor
+	ldr r3, [r2] 						; load value at divisor table
+if1
+	cmp r0, r3							; if(numberBeingConverted >= divisor){
+	blt endif1
+	ldr r5, =0 							; digitCount = 0;
+while
+	cmp r0, r3							; while(numberBeingConverted >= divisor){
+	blt endwh
+	sub r0, r0, r3						; numberBeingConverted -= divisor
+	add r5, r5, #1						; digitCount++
+	b while
+endwh
+	;put decimal number into memory
+	str r5, [r1]						; store digit to memory space
+	ldr r5, =0							; digitCount = 0
+	add r1, r1, #4						; digitAddress++
+endif1
+	add r4, r4, #1						; index++
+	add r2, r2, #4						; divisorTableAddress++
+	b for
+endfor
+	;terminate the string
+	ldr r5, =-1							; load -1 as terminating character			
+	str r5, [r1]						; store to digitAddress
+	ldmfd SP!, {pc, r3-r8}				
 		
 	AREA	Table, DATA, READONLY
 			
@@ -77,8 +111,11 @@ DIVISORTABLE
 	DCD 10
 	DCD 1
 		
-	AREA	AsciiConversion, DATA, READWRTIE
+	AREA	AsciiConversion, READWRITE
 		
 ASCIIREPRESENTATION SPACE 12
+	
+	END
+	
 
 	
