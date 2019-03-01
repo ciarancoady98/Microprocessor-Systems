bf3eb9c03bcd2aa23b996170adff7d8602a519aa
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-03-01 10:33:56 +0000

    Added some comments
    added comments to help debug in the future

--- Assignment 3/App.s
+++ Assignment 3/App.s
@@ -7,32 +7,44 @@
 	EXPORT	start
 start
 
-IO1PIN	EQU	0xE0028010
-IO1DIR	EQU	0xE0028018
-IO1SET	EQU	0xE0028014
-IO1CLR	EQU	0xE002801C
+; INTIALISATION
+;
+;
+;
+IO1PIN	EQU	0xE0028010							;I/O Pin Register Address
+IO1DIR	EQU	0xE0028018							;Set Direction Of Pins
+IO1SET	EQU	0xE0028014							;Set Bits In Register (Turn off LEDS)
+IO1CLR	EQU	0xE002801C							;Clear Bits In Register (Turn on LEDS)
 
 	ldr	r1,=IO1DIR
-	ldr	r2,=0x000f0000	;select P1.19--P1.16
-	str	r2,[r1]		;make them outputs
+	ldr	r2,=0x000f0000							;Select P1.19--P1.16
+	str	r2,[r1]									;Make them outputs
 	ldr	r1,=IO1SET
-	str	r2,[r1]		;set them to turn the LEDs off
-	ldr r11, =SUM ;sum in mem
-	str r11, [r11]
+	str	r2,[r1]									;Turn the LEDs off (Set bits)
+	ldr r11, =SUM 								;Address in memory where we store our "stack"
+	;str r11, [r11]								;???
+	mov r12, #0									;R12 - number we are currently working on
 	
-	;R12 - number we are currently working on
-	mov r12, #0
+	
+;MAIN PROGRAM LOOP
+;
+;
+;
 mainloop
-	;bl press
-	ldr r0, =0x00800000
-	cmp r0, #0
-	beq endSwitch
-	cmp r0, #0x00400000
+	;bl press									;Poll to see if button has been pressed
+	ldr r0, =0x00800000							;Comparison value for Increase Current Number
+	cmp r0, #0									;if(press() != notPressed)
+	beq endSwitch								
+	cmp r0, #0x00400000							;Comparison value for Decrease Current Number
 	bge numberChange
 	b operatorChange
 endSwitch
 	b mainloop
-
+	
+;BRANCH AND LINK TO SUBROUTINES
+;Recuired as we need to branch and link
+;Can get rid of this by calculating offset with the pc
+;
 numberChange
 	;mov r1, r12
 	bl numberChangeSub
@@ -45,75 +57,91 @@ operatorChange
 
 stop	B	stop
 
+;numberChange Subroutine
+;
 ;R0 - button pressed
 ;R12 - current number
+;
+;Increses or decreases the current sum based
+;on the button pressed
 numberChangeSub
-	stmfd sp!, {lr}
-	cmp r0, #0x00800000
-	beq adding
+	stmfd sp!, {lr}								;Save link register to stack
+	cmp r0, #0x00800000							;if(buttonPressed != '+')
+	beq adding	
 	;subtracting
-	sub r12, r12, #1
+	sub r12, r12, #1							;currentNumber--
 	b endNumberChange
-adding
-	add r12, r12, #1
-	
+adding											;else if(buttonPressed == '-')
+	add r12, r12, #1							;currentNumber++
 endNumberChange
-	
 	mov r1, r12
-	bl updateDisplay
+	bl updateDisplay							
 	ldmfd sp!, { pc}
 	
+	
+;operatorChange Subroutine
+;
+;R0 - button pressed
+;R12 - current number
+;
+;Adds a plus or minus to our stack based
+;on the button pressed
 operatorChangeSub
-	stmfd sp!, {lr}
-	str r12, [r11] ; store num
-	add r11, r11, #4 ; inc address
-	cmp r0, #0x00200000 ; +
+	stmfd sp!, {lr}						
+	str r12, [r11] 								;Store current number to our "stack"
+	add r11, r11, #4 							;Increment stack address (full ascending stack)
+	cmp r0, #0x00200000 						;if(operator != '+')
 	beq addition
 	;subtraction
-	ldr r3, ='-'
-	str r3, [r11]
+	ldr r3, ='-'						
+	str r3, [r11]								;pushToStack('-')
 	b endOpChange
-
 addition
-	ldr r3, ='+'
-	str r3, [r11]
+	ldr r3, ='+'								;else if(operator == '+')
+	str r3, [r11]								;pushToStack('+')
 endOpChange
-	add r11, r11, #4 ; inc adr
+	add r11, r11, #4 							;Increment stack address
 	ldmfd sp!, { pc}
-	
+
+;press subroutine
+;polls the I/O pin register to see if a 
+;button has been pressed
 press
 	stmfd sp!, {lr}
-	ldr r0, =IO1PIN
-	ldr r0, [r0]
-	ldr r1, =0x00f00000
-	and r0, r0, r1
-	mvn r0, r0
-	and r0, r0, r1
+	ldr r0, =IO1PIN													
+	ldr r0, [r0]								;Poll Pin Register
+	ldr r1, =0x00f00000							;Mask for Button bits
+	and r0, r0, r1								;Mask out Button bits
+	mvn r0, r0									;Invert all bits so we can use button bits
+	and r0, r0, r1								;Mask out other bits we don't need
 	ldmfd sp!, {pc}
 	
+;clearDisplay subroutine
+;Turns off all LEDS
 clearDisplay
 	stmfd sp!, {r0-r7, lr}
 	ldr	r1,=IO1SET
-	str	r0,[r1]		;set them to turn the LEDs off
+	str	r0,[r1]									;Turn the LEDs off
 	ldmfd sp!, {r0-r7, pc}
 	
+;updateDisplay subroutine
+;Updates the value displayed on the LEDS
 updateDisplay
 	stmfd sp!, {r0-r7, lr}
-	mov r0, r1, lsl #16
+	mov r0, r1, lsl #16							;Shift currentNumber to the correct position to mask
 	ldr	r1,=IO1SET
 	mov r2, #0x000f0000
-	str	r2,[r1]		;set them to turn the LEDs off
+	str	r2,[r1]									;Turn the LEDs off
 	
-	ldr	r5,=40000000
+	ldr	r5,=40000000							;Value for delay
 dloop
 	cmp r5, #0								
-	ble	enddloop						; while(delay > 0){
-	subs r5,r5,#1						; 	delay--
-	b dloop	
-	
+	ble	enddloop								;while(delay > 0){
+	subs r5,r5,#1								; 		delay--
+	b dloop										;}
 enddloop
 	ldr	r1,=IO1CLR
-	str	r0,[r1]		;turn on the LED's
+	str	r0,[r1]									;Turn on correct LED's
 	ldmfd sp!, {r0-r7, pc}
 
 	
