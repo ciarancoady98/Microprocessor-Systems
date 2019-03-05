c7a87494c214bce75b8eec4bed0283d64f79e3fe
Ciaran Coady <46931188+ciarancoady98@users.noreply.github.com>
2019-03-05 21:45:57 +0000

    90% sure its working
    Fixed a load of little bugs with registers being the wrong values and what not, probably caused by us being tired and stupid xD
    Seems to work fine in the debugger as far as i can tell.
    
    Further testing required on actual hardware.

--- Assignment 3/App.s
+++ Assignment 3/App.s
@@ -18,49 +18,53 @@ IO1CLR	EQU	0xE002801C							;Clear Bits In Register (Turn on LEDS)
 
 	ldr	r1,=IO1DIR
 	ldr	r2,=0x000f0000							;Select P1.19--P1.16
-	str	r2,[r1]								;Make them outputs
+	str	r2,[r1]									;Make them outputs
 	ldr	r1,=IO1SET
 	str	r2,[r1]									;Turn the LEDs off (Set bits)
 	mov r12, #0									;R12 - number we are currently working on
-	mov r11, #0								;R11 - sum
-	mov r10, #0								;R10 - operator
-	mov r9, #0 								;R9 - last number
-	mov r8, #0								;R8 - last button
+	mov r11, #0									;R11 - sum
+	mov r10, #0									;R10 - operator
+	mov r9, #0 									;R9 - last number
+	mov r8, #0									;R8 - last button
 	
 ;MAIN PROGRAM LOOP
 ;
 ;
 ;
-	
-	mov r6, #0
 mainloop
-	;bl flash
 	ldr r0, =0
-	b pressCheck		;Poll to see if button has been pressed
+	b pressCheck								;Poll to see if button has been pressed
 endPressCheck
-	cmp r0, #0								;if(press() != notPressed)
+	cmp r0, #0									;if(press() != notPressed)
 	beq endSwitch								
 	cmp r0, #0x00200000							;Comparison value for Decrease Current Number
 	ble numberChange
 	b operatorChange
 endSwitch
-	
+	cmp r7, #0
+	beq endMain
+	mov r7, #0									;Reset clear flag
+endMain
 	b mainloop
 	
 ;BRANCH AND LINK TO SUBROUTINES
-;Recuired as we need to branch and link
+;Required as we need to branch and link
 ;Can get rid of this by calculating offset with the pc
 ;
+
+;Branch to the press subroutine
 pressCheck
 	bl press
-	cmp r7, #1
-	beq endSwitch
-	b endPressCheck
-
+	cmp r7, #1									;check if we just cleared
+	beq endSwitch								;if we just cleared skip normal operation check
+	b endPressCheck								;if we didn't clear anything find out what operation it is
+	
+;Branch to numberChange subroutine
 numberChange
 	bl numberChangeSub
 	b endSwitch
 	
+;Branch to numberChange subroutine
 operatorChange
 	bl operatorChangeSub
 	b endSwitch
@@ -76,13 +80,13 @@ stop	B	stop
 ;on the button pressed
 numberChangeSub
 	stmfd sp!, {lr}								;Save link register to stack
-	mov r8, #0
+	mov r8, #0									;last button = number
 	cmp r0, #0x00100000							;if(buttonPressed != '+')
 	beq adding	
 	;subtracting
 	sub r12, r12, #1							;currentNumber--
 	b endNumberChange
-adding										;else if(buttonPressed == '-')
+adding											;else if(buttonPressed == '-')
 	add r12, r12, #1							;currentNumber++
 endNumberChange
 	mov r1, r12
@@ -99,42 +103,36 @@ endNumberChange
 ;on the button pressed
 operatorChangeSub
 	stmfd sp!, {lr}						
-	;poll for long press
-	;bl longPress
-	;cmp r7, #1						;if(!long press)
-	;beq endOpChange2PointO
-	mov r8, #1						;last button = operator
-	cmp r10, #0						;if(firstOp)
+	mov r8, #1									;last button = operator
+	cmp r10, #0									;if(firstOp) WE ARE PROBABLY UPDATING R10 AND THIS DOESNT WORK ANYMORE AS A RESULT
 	beq firstNumber
 	b endFirstNumber
 firstNumber
-	mov r11, r12						;sum = currentSum
+	mov r11, r12								;sum = currentNumber
 	b updateCurrentOp
 endFirstNumber
 	cmp r10, #'+'						
 	bne minus
-	add r11, r11, r12					;add
+	add r11, r11, r12							;add
 	b updateCurrentOp
 minus
-	sub r11, r11, r12					;sub
+	sub r11, r11, r12							;sub
 
 
 	
 updateCurrentOp
-	cmp r0, #0x00400000 					;if(operator != '+')
+	cmp r0, #0x00400000 						;if(operator != '+')
 	beq addition
 	;subtraction
-	ldr r10, ='-'						;operator = -
-	mov r9, r12	
+	ldr r10, ='-'								;operator = -
+	mov r9, r12									;lastNumber = currentNumber
 	b endOpChange
 addition
-	ldr r10, ='+'						;else if(operator == '+')
-	mvn r9, r12						;last number = -currentNumber
-	add r9, r9, #1
-						;last number = currentNumber
+	ldr r10, ='+'								;else if(operator == '+')
+	mvn r9, r12									
+	add r9, r9, #1								;last number = -currentNumber
 endOpChange
-	mov r12, #0						;currentNumber = 0
-endOpChange2PointO
+	mov r12, #0									;currentNumber = 0 DO I NEED THIS???
 	mov r1, r11
 	bl updateDisplay
 	ldmfd sp!, { pc}
@@ -149,56 +147,57 @@ press
 	ldr r1, =0x00f00000							;Mask for Button bits
 	and r0, r0, r1								;Mask out Button bits
 	mvn r0, r0									;Invert all bits so we can use button bits
-	and r0, r0, r1	;Mask out other bits we don't need
-	cmp r0, #0x00400000
-	bge branchToLongPress
+	and r0, r0, r1								;Mask out other bits we don't need
+	cmp r0, #0x00400000							;if(operatorPressed)
+	bge branchToLongPress						;check for long press
 endLongPressBranch
 	ldmfd sp!, {pc}
 	
+;Branch to long press subroutine
 branchToLongPress
 	bl longPress
 	b endLongPressBranch
 	
 ;long press subroutine
 ;polls the I/O pin register to see if a 
-;button has been pressed
+;button has been long pressed
 longPress
 	stmfd sp!, {r0,r6,lr}
-	mov r5,#0
-	ldr r6, =50000000
-dloop3
-	cmp r5, r6
+	mov r5,#0									;pressDuration
+	ldr r6, =50000000							;maxLongPress
+	mov r7, r0									;tempButton = buttonPressed
+dloop3	
+	cmp r5, r6									;while(pressDuration < maxLongPress)
 	beq enddloop3
-	cmp r0, #0x00400000								
-	blt enddloop3	;while(delay > 0 && button=pressed){
-	;bl flash
+	cmp r0, #0x00400000							;if(buttonPressed == '+' || buttonPressed == '-')						
+	blt enddloop3
 	ldr r0, =IO1PIN												
 	ldr r0, [r0]								;Poll Pin Register
 	ldr r1, =0x00f00000							;Mask for Button bits
 	and r0, r0, r1								;Mask out Button bits
-	mvn r0, r0								;Invert all bits so we can use button bits
+	mvn r0, r0									;Invert all bits so we can use button bits
 	and r0, r0, r1								;Mask out other bits we don't need
 updateButton
-	cmp r0, #0
+	cmp r0, #0									;if(buttonPressed)
 	beq endUpdateButton
-	mov r7, r0
+	mov r7, r0									;tempButton = buttonPressed
 endUpdateButton
 	
-	add r5,r5,#1								; 		delay--
+	add r5,r5,#1								;pressDuration++
 	b dloop3									;
 enddloop3
 	
-	ldr r6, =500000
-	cmp r5, r6
+	ldr r6, =500000								;longPress comparison value
+	cmp r5, r6									;if(pressDuration >= comparisionValue
 	blt endLongPress
-	cmp r7, #0x00800000
-	beq clearAll
-	bl clearLastSub
+	cmp r7, #0x00800000							
+	beq clearAll								;if(operator == '-')
+	bl clearLastSub								;if(operator == '+')
 endClear
-	mov r7, #1
+	mov r7, #1									;lastOperation = clear
 	b theActualEnd
 endLongPress
-	mov r7, #0
+	mov r7, #0									;lastOperation = not clear
 theActualEnd
 	mov r1, r11
 	bl updateDisplay
@@ -211,55 +210,23 @@ clearAll
 clearAllSub
 	stmfd sp!, {lr}
 	mov r12, #0									;R12 - number we are currently working on
-	mov r11, #0								;R11 - sum
-	mov r10, #0								;R10 - operator
-	mov r9, #0 								;R9 - last number
-	mov r8, #0								;R8 - last button
+	mov r11, #0									;R11 - sum
+	mov r10, #0									;R10 - operator
+	mov r9, #0 									;R9 - last number
+	mov r8, #0									;R8 - last button
 	ldmfd sp!, {pc}
 	
 clearLastSub
 	stmfd sp!, {lr}
-	cmp r8, #0
+	cmp r8, #0									;if(lastButton == number)
 	beq numberLast
-	add r11, r11, r9
+	add r11, r11, r9							;update sum
 numberLast
 	mov r10, #0								
 	mov r9, #0 
 	mov r8, #0
 	mov r12, #0
 	ldmfd sp!, {pc}
-	
-;clearDisplay subroutine
-;Turns off all LEDS
-flash
-	stmfd sp!, {r0-r7, lr}
-	ldr	r1,=IO1CLR
-	mov r0, #0x000f0000
-	str	r0,[r1]		;Turn the LEDs off
-	ldr r5, =500000
-dloop12
-	cmp r5, #0								
-	ble	enddloop12								;while(delay > 0){
-	subs r5,r5,#1								; 		delay--
-	b dloop12									;}
-enddloop12
-	ldr	r1,=IO1SET
-	str	r0,[r1]	
-	
-	ldr	r1,=IO1CLR
-	mov r0, #0x000f0000
-	str	r0,[r1]		;Turn the LEDs off
-	ldr r5, =500000
-dloop17
-	cmp r5, #0								
-	ble	enddloop17								;while(delay > 0){
-	subs r5,r5,#1								; 		delay--
-	b dloop17									;}
-enddloop17
-	ldr	r1,=IO1SET
-	str	r0,[r1]	
-
-	ldmfd sp!, {r0-r7, pc}
 	
 ;updateDisplay subroutine
 ;Updates the value displayed on the LEDS
